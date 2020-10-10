8fcec577137cd5650b3b094b43d76a0fd4b5b81e
SANDEEP RAWAT1 <SANDEEP RAWAT@SANDEEP>
2020-10-10 20:19:18 +0530

    from eclipse

--- src/hello/Test.java
+++ src/hello/Test.java
@@ -6,6 +6,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println(getRandomNumber());
+		System.out.println(Math.random() + " " + Math.random());
 	}
 	
 	private static int getRandomNumber() {
