1f589204bbb3e59596fa551860ba641747fc47eb
SANDEEP RAWAT <SANDEEP RAWAT@SANDEEP>
2020-10-10 20:35:53 +0530

    from sts, added one Hello class

---
+++ src/hello/Hello.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class Hello {
+	
+	private String message;
+	
+	public Hello(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+	
+}
