b1a18794e3b479fe13dddcd00fbec00396e9cdb6
Sandeep1 <sandeep@example.com>
2020-10-10 20:11:42 +0530

    from eclipse jee, added one method name getRandomeNumber().

--- src/hello/Test.java
+++ src/hello/Test.java
@@ -1,7 +1,14 @@
 package hello;
 
+import java.util.Random;
+
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println(getRandomNumber());
+	}
+	
+	private static int getRandomNumber() {
+		return new Random().nextInt();
 	}
 }
